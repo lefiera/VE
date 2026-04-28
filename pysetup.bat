80b4e83df5dca34d9bcef1f9421fa87bc7bdf98f
lefiera <sengfeipc@gmail.com>
2026-04-28 14:23:38 -0700

    -

--- pysetup.bat
+++ pysetup.bat
@@ -1,6 +1,7 @@
 @echo off
 setlocal enabledelayedexpansion
-set "main=C:\ProgramData\VoltEnhanced"
+
+set "maindir=C:\ProgramData\VoltEnhanced"
 
 if defined PROCESSOR_ARCHITEW6432 (
     set "arch=!PROCESSOR_ARCHITEW6432!"
@@ -13,21 +14,21 @@ if /i "!arch!"=="ARM64" set "pyarch=arm64"
 for /f %%i in ('powershell -NoProfile -Command "(Invoke-RestMethod -Uri 'https://endoflife.date/api/python.json')[0].latest"') do set "pyver=%%i"
 
 set "installedver="
-if exist "%main%\py\python.exe" (
-    for /f "tokens=2" %%v in ('"%main%\py\python.exe" --version 2^>^&1') do set "installedver=%%v"
+if exist "%maindir%\py\python.exe" (
+    for /f "tokens=2" %%v in ('"%maindir%\py\python.exe" --version 2^>^&1') do set "installedver=%%v"
     if "!installedver!" neq "%pyver%" (
-        rmdir /s /q "%main%\py"
+        rmdir /s /q "%maindir%\py"
         call :pyinstall
     )
 ) else (
-    if exist "%main%\py" (
-        rmdir /s /q "%main%\py"
+    if exist "%maindir%\py" (
+        rmdir /s /q "%maindir%\py"
     )
     call :pyinstall
 )
 
-if exist "%main%\py\python.exe" (
-    echo %main%\py\python.exe
+if exist "%maindir%\py\python.exe" (
+    echo %maindir%\py\python.exe
     exit /b 0
 ) else (
     exit /b 1
@@ -35,5 +36,5 @@ if exist "%main%\py\python.exe" (
 
 ::funcs
 :pyinstall
-powershell -NoProfile -Command "$url='https://www.python.org/ftp/python/%pyver%/python-%pyver%-embed-%pyarch%.zip'; $zip=Join-Path $env:TEMP 'python_embed.zip'; Invoke-WebRequest -Uri $url -OutFile $zip; Expand-Archive -Path $zip -DestinationPath '%main%\py' -Force; Remove-Item $zip"
+powershell -NoProfile -Command "$ProgressPreference='SilentlyContinue'; $url='https://www.python.org/ftp/python/%pyver%/python-%pyver%-embed-%pyarch%.zip'; $zip=Join-Path $env:TEMP 'python_embed.zip'; Invoke-WebRequest -Uri $url -OutFile $zip; Expand-Archive -Path $zip -DestinationPath '%maindir%\py' -Force; Remove-Item $zip"
 goto :eof
