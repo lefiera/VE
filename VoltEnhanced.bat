430ff8939f161dde4573006021cdcd558698903d
lefiera <sengfeipc@gmail.com>
2026-04-28 14:53:36 -0700

    Update VoltEnhanced.bat

--- VoltEnhanced.bat
+++ VoltEnhanced.bat
@@ -5,7 +5,7 @@ set "maindir=C:\ProgramData\VoltEnhanced"
 
 if exist "%maindir%\pysetup.bat" (
     for /f "delims=" %%i in ('call "%maindir%\pysetup.bat"') do set "py=%%i"
-    echo python: !py!
+    echo python path: !py!
 ) else (
     echo did the installer mess up? "%maindir%\pysetup.bat" doesnt exist.
 )
