0d28bee0334703c4d727fb8860c554304cc3153e
lefiera <sengfeipc@gmail.com>
2026-04-28 14:33:26 -0700

    -

---
+++ VoltEnhanced.bat
@@ -0,0 +1,10 @@
+@echo off
+setlocal enabledelayedexpansion
+
+set "maindir=C:\ProgramData\VoltEnhanced"
+
+if exist "%maindir%\pysetup.bat" (
+    call "%maindir%\pysetup.bat"
+) else (
+    echo did the installer mess up? the required file(s) dont exist.
+)
