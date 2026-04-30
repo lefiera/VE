087f67a22b06a0768c54ce46554c0ac3817a5b96
lefiera <sengfeipc@gmail.com>
2026-04-30 11:08:59 -0700

    Update main.py

--- src/main.py
+++ src/main.py
@@ -1,6 +1,6 @@
 import os, webview
 os.environ['WEBVIEW2_BROWSER_EXECUTABLE_FOLDER'] = "C:\ProgramData\VoltPlus\webview"
 
-#start window
+#start main window
 window = webview.create_window('VoltEnhanced', 'https://example.com')
 webview.start(storage_path=None, private_mode=False)
